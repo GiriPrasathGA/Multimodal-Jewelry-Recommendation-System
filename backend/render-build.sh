ff1df5279f4d062c190c09f1f277437c1bea1e1e
GIRI PRASATH G A <giriprasath7806@gmail.com>
2026-02-18 14:53:52 +0530

    chore: remove unused render build script

--- backend/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-pip install --upgrade pip
-# Install CPU-only torch to save space (approx 700MB -> 150MB)
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-pip install -r requirements.txt
